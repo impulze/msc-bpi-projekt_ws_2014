01681fd0564a9bcdf0c0daaa8ad7cb103030f662
Eric Reichling <Shishateur@gmail.com>
2015-01-29 17:10:54 +0100

    Website mit Product.new() stimmt jetzt mit XML-Feldern überein

--- Website/js/test.js
+++ Website/js/test.js
@@ -15,17 +15,17 @@ var add = function(){
 		var infourl = document.getElementById('infourl').value;
 
 		document.getElementById('area').value =":gtin => "
-		+gtin+", :provider_gln => 2865195100007, :provider_name => 'Duff Brewery', :ttl => 'P1D', :variant_desc =>"
-		+variante+", :variant_dtime => '2014-01-01T00:00:00Z', :product_name_de => "
-		+name+", :product_name_en => "
-		+nameEN+", :marketing_description => "
-		+werbespruch+", :regulated_product_name => "
-		+sorte+", :regulated_product_name_en => "
-		+sorteEN+", :information_link => "
-		+infourl+", :img_link => "
-		+bildurl+", :ingriedient_statement => "
-		+inhaltsangabe+", :ingriedient_name =>"
-		+inhaltsstoffe+", :percentageOfAlcoholByVolume => "+alkg;
+		+gtin+", :informationProviderGLN => 2865195100007, :informationProviderName => 'Duff Brewery', :timeToLive => 'P1D', :productionVariantDescription => '"
+		+variante+"', :productVariantEffectiveDateTime => '2014-01-01T00:00:00Z', :productNameDE => '"
+		+name+"', :productNameEN => '"
+		+nameEN+"', :consumerMarketingDescription => '"
+		+werbespruch+"', :regulatedProductName => '"
+		+sorte+"', :regulatedProductNameEN => '"
+		+sorteEN+"', :productInformationLink => '"
+		+infourl+"', :imageLink => '"
+		+bildurl+"', :ingriedientStatement => '"
+		+inhaltsangabe+"', :ingriedientName => '"
+		+inhaltsstoffe+"', :percentageOfAlcoholByVolume => "+alkg;
 };
 
 function neu(){
@@ -41,4 +41,23 @@ function neu(){
 		document.getElementById('inhaltsangabe').value = "Malz und Hopfen und so";
 		document.getElementById('bildurl').value = "http://example.com/duff.jpg";
 		document.getElementById('infourl').value = "http://example.com/duff";
+}
+
+function array(){
+		console.log("Test");
+		var gtin = document.getElementById('gtin').value;
+		var variante = document.getElementById('variante').value;
+		var name = document.getElementById('name').value;
+		var nameEN = document.getElementById('nameEN').value;
+		var werbespruch = document.getElementById('werbespruch').value;
+		var sorte = document.getElementById('sorte').value;
+		var sorteEN = document.getElementById('sorteEN').value;
+		var alkg = document.getElementById('alkoholgehalt').value;
+		var inhaltsstoffe = document.getElementById('inhaltsstoffe').value;
+		var inhaltsangabe = document.getElementById('inhaltsangabe').value;
+		var bildurl = document.getElementById('bildurl').value;
+		var infourl = document.getElementById('infourl').value;
+		
+		var result = [gtin,variante,name,nameEN,werbespruch,sorte,sorteEN,alkg,inhaltsstoffe,inhaltsangabe,bildurl,infourl];
+		console.log("Test");	
 }
